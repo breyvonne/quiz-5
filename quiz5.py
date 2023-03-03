1045fb5b859faeaeb89cb55d3d548bb237099c34
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-03-03 11:51:10 -0500

    Add files via upload

---
+++ quiz5.py
@@ -0,0 +1,28 @@
+#Breanna Eafon
+def tire_pressure(pres):
+    if pres>28:
+        print("Tire pressure is low")
+    else:
+        print("None")
+#problem 2
+def thermostat(temp):
+    if temp<= 52:
+        print("The heater is on")
+    elif 52<temp<71 :
+        print("Heater and AC are off")
+    else:
+        print("AC is on")
+
+    #problem 3
+def smoothie(fruit):
+    if fruit=="banana":
+        print("Banana it is")
+    if fruit=="cherry":
+        print("I cherish you")
+    if fruit=="apple":
+        print("I applesolutely like you!")
+    if fruit=="orange":
+        print("Orange you gladto see me?")
+    if fruit=="melon":
+        print("You are one in a melon")
+smoothie("banana")
